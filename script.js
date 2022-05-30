e89bb85af54e032216783d461f4914473c31443e
Andy Gorbould <gorbould@mail.com>
2022-05-30 14:37:54 +0200

    Scarlet/Violet modes working

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // import & call "carousel-script.js" function
 import { carousel } from "./carousel-script.js";
 carousel();
+
+// function dark() {
+
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+// }
