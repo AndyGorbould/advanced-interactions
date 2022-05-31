ffe86be67ae21dbc0b07322bacb670084c322a75
Andy Gorbould <gorbould@mail.com>
2022-05-31 16:41:48 +0200

    runner working!

--- script.js
+++ script.js
@@ -4,27 +4,40 @@ carousel();
 // import { imageDisplay } from "./image-display.js";
 // imageDisplay();
 
-
+////////////////////////////////////////////
+// Chaser
+////////////////////////////////////////////
 let boxA = document.getElementById("boxA");
 let chaser = document.getElementsByClassName("chaser")[0];
 
 boxA.addEventListener("mousemove", function (e) {
 
-    let x = e.offsetX-25;
-    let y = e.offsetY-25;
+    let x = e.offsetX - 25;
+    let y = e.offsetY - 25;
 
 
     chaser.style.left = x + "px";
-    chaser.style.top = y + "px";    
+    chaser.style.top = y + "px";
 
 });
 
-// chaser.addEventListener("mousemove", function (e) {
+////////////////////////////////////////////
+// Runner
+////////////////////////////////////////////
+let boxB = document.getElementById("boxB");
+let runner = document.getElementsByClassName("runner")[0];
 
-//     let x = e.offsetX-25;
-//     let y = e.offsetY-25;
 
-//     chaser.style.left = x + "px";
-//     chaser.style.top = y + "px";    
+boxB.addEventListener("mousemove", function (e) {
 
-// });
+
+    let rect = boxB.getBoundingClientRect();
+
+    let x = boxB.clientWidth - (e.offsetX - rect.x) - 250;
+    let y = boxB.clientHeight - (e.offsetY - rect.y) - 1200;
+
+
+    runner.style.left = x + "px";
+    runner.style.top = y + "px";
+
+});
