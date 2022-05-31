fe313345b89340fcc54823ef4f13d4b507cda6f6
Andy Gorbould <gorbould@mail.com>
2022-05-31 16:11:50 +0200

    chaser is working 90%

--- script.js
+++ script.js
@@ -1,64 +1,30 @@
 // import & call "carousel-script.js" function
 import { carousel } from "./carousel-script.js";
 carousel();
+// import { imageDisplay } from "./image-display.js";
+// imageDisplay();
 
 
-let pokePigeot = document.getElementById('poke-pigeot')
-let pokePigeotHidden = document.getElementsByClassName("poke-hidden")[0];
+let boxA = document.getElementById("boxA");
+let chaser = document.getElementsByClassName("chaser")[0];
 
-pokePigeot.addEventListener("mouseenter", hoverImg1);
-pokePigeot.addEventListener("mouseleave", hoverImgLeave1);
+boxA.addEventListener("mousemove", function (e) {
 
-function hoverImg1() {
+    let x = e.offsetX-25;
+    let y = e.offsetY-25;
 
-    pokePigeotHidden.style.opacity = 1;
-};
-function hoverImgLeave1() {
 
-    pokePigeotHidden.style.opacity = 0;
-};
-///////////////
-let pokeMagikarp = document.getElementById('poke-magikarp')
-let pokeMagikarpHidden = document.getElementsByClassName("poke-hidden")[1];
+    chaser.style.left = x + "px";
+    chaser.style.top = y + "px";    
 
-pokeMagikarp.addEventListener("mouseenter", hoverImg2);
-pokeMagikarp.addEventListener("mouseleave", hoverImgLeave2);
+});
 
-function hoverImg2() {
+// chaser.addEventListener("mousemove", function (e) {
 
-    pokeMagikarpHidden.style.opacity = 1;
-};
-function hoverImgLeave2() {
+//     let x = e.offsetX-25;
+//     let y = e.offsetY-25;
 
-    pokeMagikarpHidden.style.opacity = 0;
-};
-///////////////
-let pokeSlowpoke = document.getElementById('poke-slowpoke')
-let pokeSlowpokeHidden = document.getElementsByClassName("poke-hidden")[2];
+//     chaser.style.left = x + "px";
+//     chaser.style.top = y + "px";    
 
-pokeSlowpoke.addEventListener("mouseenter", hoverImg3);
-pokeSlowpoke.addEventListener("mouseleave", hoverImgLeave3);
-
-function hoverImg3() {
-
-    pokeSlowpokeHidden.style.opacity = 1;
-};
-function hoverImgLeave3() {
-
-    pokeSlowpokeHidden.style.opacity = 0;
-};
-///////////////
-let pokeFarfetchd = document.getElementById('poke-farfetchd')
-let pokeFarfetchdHidden = document.getElementsByClassName("poke-hidden")[3];
-
-pokeFarfetchd.addEventListener("mouseenter", hoverImg4);
-pokeFarfetchd.addEventListener("mouseleave", hoverImgLeave4);
-
-function hoverImg4() {
-
-    pokeFarfetchdHidden.style.opacity = 1;
-};
-function hoverImgLeave4() {
-
-    pokeFarfetchdHidden.style.opacity = 0;
-};
+// });
